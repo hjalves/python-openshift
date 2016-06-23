923b7cc1419dec1eb8d77b7e040224a76b92db3e
Humberto Alves <halves@uc.pt>
2016-06-23 16:47:04 +0100

    First commit.

---
+++ app.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import os
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello World!")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+port = int(os.environ.get('OPENSHIFT_PYTHON_PORT', '8000'))
+ip = os.environ.get('OPENSHIFT_PYTHON_IP', 'localhost')
+
+if __name__ == "__main__":
+    application.listen(port, ip)
+    tornado.ioloop.IOLoop.current().start()
